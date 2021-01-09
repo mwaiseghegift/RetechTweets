e6a80d9c4ba2e6cb8075ff645f16c0325c62b0db
Gateway47 <itsregalo047@gmail.com>
2021-01-09 23:35:25 +0300

    DRF

--- tweets/views.py
+++ tweets/views.py
@@ -5,8 +5,12 @@ from .forms import TweetForm
 from django.utils.http import is_safe_url
 from django.conf import settings
 from .models import Tweet
-from .serializers import TweetSerializer
 
+#rest framework
+from rest_framework.response import Response
+from .serializers import TweetSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
@@ -21,6 +25,59 @@ def HomepageView(request):
 
     return render(request, 'home.html', data)
 
+@api_view(['POST'])  #http method is POST
+@permission_classes([IsAuthenticated])
+def TweetCreateView(request, *args, **kwargs):
+    #Django Rest Framework CreateView
+    serializer = TweetSerializer(data=request.POST)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user)
+        return Response(serializer.data, status = 201)
+    return Response({}, status = 400)
+    
+    
+    #Normal CreateView
+    """
+    user = request.user
+    if not request.user.is_authenticated:
+        user = None
+        if request.is_ajax():
+            return JsonResponse({}, status=401) #not authorized
+        return redirect(settings.LOGIN_URL)
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.user = user
+        obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201)
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
+        form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+    data = {
+        'form':form,
+    }
+
+    return render(request, 'components/tweetform.html', data)
+    """
+
+
+
+#DRF list view
+
+@api_view(['GET'])
+def TweetListView(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    serializer = TweetSerializer(qs, many=True)
+    return Response(serializer.data)
+
+
+#Pure django list_view   
+""" 
 def TweetListView(request, *args, **kwargs):
     qs = Tweet.objects.all()
     tweet_list = [i.serialize() for i in qs]
@@ -30,7 +87,18 @@ def TweetListView(request, *args, **kwargs):
     }
 
     return JsonResponse(data)
+"""
 
+@api_view(['GET'])
+def TweetDetailView(request, tweet_id, *args, **kwargs):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    obj = qs.first()
+    serializer = TweetSerializer(obj)
+    return Response(serializer.data, status = 200)
+
+"""
 def TweetDetailView(request, tweet_id, *args, **kwargs):
     data = {
         "id":tweet_id,
@@ -46,16 +114,18 @@ def TweetDetailView(request, tweet_id, *args, **kwargs):
 
     
     return JsonResponse(data, status=status)
+"""
 
+@api_view(['POST'])  #http method is POSt
 def TweetCreateView(request, *args, **kwargs):
     
     #Django Rest Framework CreateView
-    data = request.POST or None
+    data = request.POST
     serializer = TweetSerializer(data=data)
-    if serializer.is_valid():
-        obj = serializer.save(user=request.user)
-        return JsonResponse(serializer.data, status = 201)
-    return JsonResponse({}, status = 400)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user)
+        return Response(serializer.data, status = 201)
+    return Response({}, status = 400)
     
     
     #Normal CreateView
